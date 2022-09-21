afd821d26ad9f6c0269d5940b36b2ee0a4b5fe45
Ramakrishna Janapureddy <85489242+codecoder005@users.noreply.github.com>
2022-09-21 10:52:59 +0530

    initial commit

---
+++ src/main/java/com/orgofarmsgroup/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.orgofarmsgroup.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping
+    public String users(){
+        return "Welcome to GitHub Actions.";
+    }
+}
---
+++ src/test/java/com/orgofarmsgroup/githubactionsspringbootapp/GithubActionsSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orgofarmsgroup.githubactionsspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
